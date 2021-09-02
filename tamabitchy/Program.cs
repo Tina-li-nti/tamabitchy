9190eb232d0aec0e0f14b2a3643cd3c40fd9ccc3
Tina Li <tina.li@elev.ga.ntig.se>
2021-09-02 10:51:30 +0200

    fsdgjhkas

---
+++ tamabitchy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tamabitchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to tamabitchy.TM(independent from Tamagotchi.TM)");
+            Console.ReadLine();
+        }
+    }
+}
