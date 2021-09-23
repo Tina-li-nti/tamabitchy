b50b8d53e9fc42dd04aca6f3e3819ed9f793abb6
Tina Li <tina.li@elev.ga.ntig.se>
2021-09-23 11:05:53 +0200

    qwerrtr

--- tamabitchy/Program.cs
+++ tamabitchy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices.ComTypes;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.ComTypes;
 using System;
 
 namespace tamabitchy
@@ -12,7 +13,48 @@ namespace tamabitchy
             Console.WriteLine("please name you pet....");
             string name = Console.ReadLine();
             Console.WriteLine("so " + name + " it is decied then.");
+            Console.WriteLine("what would you like " + name + " to do?");
+
+
+            Tama tamagotchi = new Tama();
+            tamagotchi.name = name;
+
+            while (tamagotchi.GetAlive())
+            {
+                Console.WriteLine("Press 1 to feed " + name + ". Press 2 to teach " + name + " a word. Press 3 to let " + name + " speak. Press 4 to not do anyhting");
+                ConsoleKey key = ConsoleKey.A;
+                while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key != ConsoleKey.D3)
+                {
+                    key = Console.ReadKey().Key;
+                    if (key == ConsoleKey.D1)
+                    {
+                        tamagotchi.Feed();
+                        tamagotchi.PrintStats();
+                    }
+                    if (key == ConsoleKey.D2)
+                    {
+                        tamagotchi.Teach(Console.ReadLine());
+                        tamagotchi.PrintStats();
+                    }
+                    if (key == ConsoleKey.D3)
+                    {
+                        tamagotchi.Hi();
+                        tamagotchi.PrintStats();
+                    }
+                    if (key == ConsoleKey.D4)
+                    {
+                        tamagotchi.PrintStats();
+                    }
+
+
+
+                }
+            }
+
+
+
             Console.ReadLine();
+
         }
     }
 }
--- tamabitchy/tamaclass.cs
+++ tamabitchy/tamaclass.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System;
 
 namespace tamabitchy
@@ -6,38 +7,65 @@ namespace tamabitchy
     {
         private int hunger;
         private int boredom;
-        private bool isAlive;
-        private Random generator;
+        private List<string> words = new List<string>();
+        private bool isAlive = true;
+        private Random generator = new Random();
         public string name;
 
         public void Feed()
         {
-
+            hunger--;
         }
         public void Hi()
         {
-
+            int i = generator.Next(words.Count);
+            Console.WriteLine(words[i]);
+            ReduceBoredome();
         }
         public void Teach(string word)
         {
-
+            words.Add(word);
+            ReduceBoredome();
         }
         public void Tick()
         {
-
+            hunger++;
+            boredom++;
+            if (hunger > 11)
+            {
+                isAlive = false;
+            }
+            if (boredom > 11)
+            {
+                isAlive = false;
+            }
         }
-        public void PrintStats() 
+        public void PrintStats()
         {
+            Tick();
+            if (isAlive == true)
+            {
+                Console.WriteLine($"tamabithcy current stats: {hunger}hunger   {boredom}boredom");
+            }
+            else
+            {
+                Console.WriteLine("DEAD");
+            }
 
         }
-        public bool GetAlive() 
+        public bool GetAlive()
         {
-
+            return isAlive;
         }
-        private void ReduceBoredome() {
-            
+        private void ReduceBoredome()
+        {
+            boredom--;
+            if (boredom < 0)
+            {
+                boredom = 0;
+            }
         }
     }
-    
-    
+
+
 }
